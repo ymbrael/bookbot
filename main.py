523050ff1c8634ecf43f6913d091a997493f7129
ymbrael <ymbrael@proton.me>
2024-10-15 16:36:41 -0700

    added word counter

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    print(f"{word_count} words counted in {book_path}")
+
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()

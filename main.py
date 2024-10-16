9da831eabf4a8c601fba55ba3d32efce09d65fc2
ymbrael <ymbrael@proton.me>
2024-10-15 19:06:17 -0700

    added sorted report of letter count

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ def main():
     text = get_book_text(book_path)
     text_lc = get_text_lc(text)
     word_count = get_word_count(text)
-    print(f"{word_count} words counted in {book_path}")
-    letters_l = get_letters(text_lc)
-    letters_s = set(letters_l)
-    letter_count = get_letter_count(letters_l, letters_s)
-    print(letter_count)
+    chr_l = get_chr(text_lc)
+    chr_s = set(chr_l)
+    chr_count = get_chr_count(chr_l, chr_s)
+    chr_count.sort(reverse=True, key=sort_on)
+    ltr_count = get_alphabet_count(chr_l, chr_s)
+    ltr_count.sort(reverse=True, key=sort_on)
+
+    print(f"- Report of {book_path} -")
+    print(f"{word_count} words counted in the document")
+    print("")
+    for l in range(0, len(ltr_count)):
+        print(f"There are {ltr_count[l]['count']} instances of the letter '{ltr_count[l]['letter']}'")
+    print("- Close Report -")
 
 
 def get_word_count(text):
@@ -24,12 +32,24 @@ def get_text_lc(text):
     return text.lower()
 
 
-def get_letters(text_lc):
+def get_chr(text_lc):
     return [*text_lc]
 
 
-def get_letter_count(letters_l, letters_s):
-    return {x:letters_l.count(x) for x in letters_s}
+def get_chr_count(chr_l, chr_s):
+    return [{"character": x , "count": chr_l.count(x)} for x in chr_s]
+
+
+def conv_to_list(chr_count):
+    return list(dict.items(chr_count))
+
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def get_alphabet_count(chr_l, chr_s):
+    return [{"letter": x , "count": chr_l.count(x)} for x in chr_s if x.isalpha() == True]
 
 
 main()
